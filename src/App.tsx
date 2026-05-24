9aa7b719533dccb7255dd8b29b287e4238553356
Vinícius Gabriel <viniciusgabriel102009@gmail.com>
2026-05-23 21:32:33 -0300

    feat: add all HTML project

---
+++ src/App.tsx
@@ -0,0 +1,129 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <header>
+        <img src="" alt="Instituto Margaret Costa" />
+        <nav>
+          <a>HOME</a>
+          <a>CURSOS</a>
+          <a>CONTATO</a>
+          <a>FEEDBACKS</a>
+        </nav>
+      </header>
+
+      <div>
+        <p>SE INSCREVA</p>
+        <p>VAGAS ILIMITADAS</p>
+      </div>
+
+      <section>
+        <h2>Cursos de Beleza em Serra Talhada</h2>
+        <h1>SEU TALENTO MERECE <mark>DESTAQUE</mark></h1>
+        <a>Ver Cursos</a>
+        <img src="" alt="Modelo do Instituto Margaret Costa" />
+      </section>
+
+      <section>
+        <article>
+          <div></div>
+          <a>
+            <h3>Curso</h3>
+            <h3>Número Um</h3>
+            <p>Descrição do curso número 1: um curso desenvolvido para proporcionar aprendizado prático e estratégico.</p>
+          </a>
+        </article>
+        <article>
+          <div></div>
+          <a>
+            <h3>Curso</h3>
+            <h3>Número Dois</h3>
+            <p>Descrição do curso número 2: um curso desenvolvido para proporcionar aprendizado prático e estratégico.</p>
+          </a>
+        </article>
+        <article>
+          <div></div>
+          <a>
+            <h3>Curso</h3>
+            <h3>Número Três</h3>
+            <p>Descrição do curso número 3: um curso desenvolvido para proporcionar aprendizado prático e estratégico.</p>
+          </a>
+        </article>
+      </section>
+
+      <section>
+        <article>
+          <img src="" alt="Icone Lampada" />
+          <h5>Teoria</h5>
+        </article>
+        <article>
+          <img src="" alt="Icone Certificado" />
+          <h5>Emitimos Certificado</h5>
+        </article>
+        <article>
+          <img src="" alt="Icone Lápis" />
+          <h5>Prática</h5>
+        </article>
+      </section>
+
+      <section>
+        <article></article>
+        <a>
+          <img src="" alt="Icone Whatsapp" />
+        </a>
+        <article></article>
+        <a>
+          <h4>Nos siga no Instagram!</h4>
+          <p>@institutomargaret</p>
+        </a>
+        <iframe />
+      </section>
+
+      <section>
+        <h1>FEEDBACKS</h1>
+        <div>
+          <article>
+            <img src="" alt="Foto de Cliente" />
+            <div>
+              <h3>EXEMPLO DE CLIENTE</h3>
+              <p>Feedback do Cliente</p>
+            </div>
+          </article>
+          <article>
+            <img src="" alt="Foto de Cliente" />
+            <div>
+              <h3>EXEMPLO DE CLIENTE</h3>
+              <p>Feedback do Cliente</p>
+            </div>
+          </article>
+        </div>
+      </section>
+
+      <section>
+        <img src="" alt="Foto de Cliente" />
+        <img src="" alt="Foto de Cliente" />
+        <img src="" alt="Foto de Cliente" />
+        <img src="" alt="Foto de Cliente" />
+        <img src="" alt="Foto de Cliente" />
+        <img src="" alt="Foto de Cliente" />
+      </section>
+
+      <footer>
+        <nav>
+          <a>HOME</a>
+          <a>CURSOS</a>
+        </nav>
+        <img src="" alt="Instituto Margaret Costa" />
+        <nav>
+          <a>CONTATO</a>
+          <a>FEEDBACKS</a>
+        </nav>
+        <p>© 2026 Instituto Margaret Costa. Todos os direitos reservados.</p>
+      </footer>
+    </>
+  )
+}
+
+export default App
